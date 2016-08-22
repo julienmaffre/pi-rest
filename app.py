872e3dc9d476174c736ada09793c1dd1ae8a6b2f
julienmaffre <maffre.jul@gmail.com>
2016-08-22 21:33:28 +0100

    Changed switch on and off to PATCH and added easy entry points to switch on and off

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import logging
+import json
 
 from enum import Enum
 from flask import Flask, jsonify, request, abort, make_response
@@ -25,6 +26,7 @@ pins = [
 
 # GPIO Pin states
 PIN_HIGH_STATE = 1
+PIN_TEST = 7
 PIN_LOW_STATE = 0
 VALID_PIN_STATE = [PIN_HIGH_STATE, PIN_LOW_STATE]
 
@@ -40,25 +42,43 @@ def pi_switch_off(pin_id):
 # RESTful API functions
 #
 
-@app.route('/pins/<int:pin_id>', methods=['POST'])
+@app.route('/pins/<int:pin_id>', methods=['PATCH'])
 def set_pin(pin_id):
     pin = [pin for pin in pins if pin['id'] == pin_id]
     if len(pin) == 0:
         abort(404)
 
-    try:
-        if request.json.get('state') in VALID_PIN_STATE:
-            app.logger.debug(request.json.get('state'))
-            pin[0]['state'] = request.json.get('state')
-            if pin[0]['state'] == PIN_HIGH_STATE:
-                pi_switch_on(pin_id)
-            elif pin[0]['state'] == PIN_LOW_STATE:
-                pi_switch_off(pin_id)
-            else:
-                app.logger.debug(pin[0]['state'])
-            return jsonify({'pin': pin[0]})
-    except:
+    if request.json.get('state') in VALID_PIN_STATE:
+        app.logger.debug(request.json.get('state'))
+        pin[0]['state'] = request.json.get('state')
+        if pin[0]['state'] == PIN_HIGH_STATE:
+            pi_switch_on(pin_id)
+        elif pin[0]['state'] == PIN_LOW_STATE:
+            pi_switch_off(pin_id)
+        return jsonify({'pin': pin[0]})
+    else:
         abort(400);
+
+@app.route('/pins/<int:pin_id>/switch_on', methods=['PATCH'])
+def switch_pin_on(pin_id):
+    pin = [pin for pin in pins if pin['id'] == pin_id]
+    if len(pin) == 0:
+        abort(404)
+
+    pin[0]['state'] = 1
+    pi_switch_on(pin_id)
+    return jsonify({'pin': pin[0]})
+
+@app.route('/pins/<int:pin_id>/switch_off', methods=['PATCH'])
+def switch_pin_off(pin_id):
+    pin = [pin for pin in pins if pin['id'] == pin_id]
+    if len(pin) == 0:
+        abort(404)
+
+    pin[0]['state'] = 0
+    pi_switch_off(pin_id)
+    return jsonify({'pin': pin[0]})
+
 
 @app.route('/pins/<int:pin_id>', methods=['GET'])
 def get_pin(pin_id):
